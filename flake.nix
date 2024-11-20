649318e9e838451e41d6b4ba2a9a43c8718bf3e9
Robert Manigk <p1ng0ut@mailbox.org>
2024-11-20 21:19:29 +0100

    Remove unused systems from flake

--- flake.nix
+++ flake.nix
@@ -3,18 +3,13 @@
 
   inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
-  inputs.systems.url = "github:nix-systems/default";
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.flake-utils.inputs.systems.follows = "systems";
-
-  outputs = { self, systems, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs }:
       let
+        system = "x86_64-linux"; 
         pkgs = import nixpkgs { inherit system; };
         updateLocks = pkgs.callPackage ./update-locks.nix { };
       in {
-        devShells.default = pkgs.mkShell {
+        devShells.${system}.default = pkgs.mkShell {
           buildInputs = [
             pkgs.gradle_8
             pkgs.temurin-bin-21
@@ -22,6 +17,6 @@
             pkgs.ktlint
           ];
         };
-        packages.default = pkgs.callPackage ./build.nix { };
-      });
+        packages.${system}.default = pkgs.callPackage ./build.nix { };
+      };
 }
